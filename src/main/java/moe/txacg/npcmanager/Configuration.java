24d47adef44ebc6287e469a551a9dd759c1105b4
Txacg <fffraa119@gmail.com>
2018-01-06 04:58:34 +0800

    Add configuration and test it.

---
+++ src/main/java/moe/txacg/npcmanager/Configuration.java
@@ -0,0 +1,26 @@
+package moe.txacg.npcmanager;
+
+import cat.nyaa.nyaacore.configuration.PluginConfigure;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Configuration extends PluginConfigure{
+
+    private final NPCManager plugin;
+
+    /* Basic plugin & database config */
+    /* 基礎的插件與資料庫設定 */
+
+    @Serializable
+    public String language = "en_US";
+
+
+
+    public Configuration(NPCManager plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    protected JavaPlugin getPlugin() {
+        return plugin;
+    }
+}
--- src/main/java/moe/txacg/npcmanager/NPCManager.java
+++ src/main/java/moe/txacg/npcmanager/NPCManager.java
@@ -1,4 +1,51 @@
 package moe.txacg.npcmanager;
 
-public class NPCManager {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.HandlerList;
+
+
+public class NPCManager extends JavaPlugin{
+
+    public static NPCManager instance = null;
+    public Configuration cfg = null;
+
+    @Override
+    public void onEnable(){
+
+        instance = this;
+        cfg = new Configuration(this);
+        cfg.load();
+    }
+
+    @Override
+    public void onDisable(){
+
+        HandlerList.unregisterAll(this);
+        getServer().getScheduler().cancelTasks(this);
+        cfg.save();
+    }
+
+    public void doReload(){
+
+        HandlerList.unregisterAll(this);
+        getServer().getScheduler().cancelTasks(this);
+        onEnable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if (command.getName().equalsIgnoreCase("npcmanager") && sender instanceof Player){
+            Player player = (Player) sender;
+            player.sendRawMessage("Hi!" + player.getName() + "~");
+
+            return true;
+        }
+
+
+        return false;
+    }
 }
